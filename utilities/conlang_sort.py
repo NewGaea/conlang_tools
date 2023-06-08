2842aadfc458f32fc0a9915afce6779b88e64b81
gaeajen-6L27 <nathanielknight@newgaea.net>
2023-06-08 17:02:11 -0400

    Create conlang_sort.py

---
+++ utilities/conlang_sort.py
@@ -0,0 +1,13 @@
+'''
+The goal of this tool is to allow conlangers to use custom alphabetic
+sorting for their projects. The script imports two files, a Sorting
+List to define the order of symbols used to alphabetize and a
+Dictionary List to be sorted.
+
+The script will overwrite the Dictionary File when it is finished,
+unless it is called with the third parameter set as "--new"
+
+If called without any arguments, the script may prompt the user for
+each of the necessary files
+'''
+
